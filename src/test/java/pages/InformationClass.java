5f27eed6a28d83a2e37a33975d89a0b3e51a7a96
kemalasci <kemalasci01@gmail.com>
2022-07-29 13:50:27 +0200

    R

--- src/test/java/pages/InformationClass.java
+++ src/test/java/pages/InformationClass.java
@@ -5,11 +5,6 @@ public class InformationClass {
     public static final String webMailurl = "https://web.de/email/#.pc_page.homepage.index.nav.mail";
     public static final String mailLoginInformationen  = "kemal-asci";
     public static final String mailLoginPasword  = "Ksa6124857.";
-    public static final String errormsjString  = "Bitte füllen Sie alle Pflichtfelder aus.";
-    public static final String pdfUrl  = "https://www.usu.com/sites/usu-com/files/2021-08/usu_cs_s-t_de_web.pdf";
-    public static final String pdfKontrolString  = "Herausforderung";
-    public static final String interestedInputString  = "Starkkraft";
-    public static final String  messageInputString  = "Mit freundlichen Grüßen";
 
 
 }
